480bb42d8f1d4c83e8b87b064c2fc5434a18ce8d
Matthew Evans <mevans+git@wtfsystems.net>
2025-08-10 08:27:34 -0400

    sync

--- addons/ui_controller/ui_anchor.gd
+++ addons/ui_controller/ui_anchor.gd
@@ -1,4 +1,7 @@
 extends Node
+## A node that the UiController will anchor menus to.
+## Before calling the UiController members this node needs to be
+## placed in the main scene.
 
 func _ready() -> void:
 	UiController._ui_node = self
--- addons/ui_controller/ui_controller.gd
+++ addons/ui_controller/ui_controller.gd
@@ -1,15 +1,19 @@
 extends Node
+## A singleton class that controls which menus are attached on the UiAnchor node.
 
 var _ui_node: Node
 var _ui_ref: Array[String]
 
 @export var _menus: Dictionary[String, PackedScene]
 
+## Opensa menu.
+## Takes a string name of a menu scene and attaches it to the UiAnchor node.
 func open_menu(menu_name: String) -> void:
 	_clear_menu_mem()
 	_ui_ref.push_back(menu_name)
 	_ui_node.add_child(_menus[menu_name].instantiate())
 
+## Close menu
 func close_menu() -> void:
 	_clear_menu_mem()
 	_ui_ref.pop_back()
