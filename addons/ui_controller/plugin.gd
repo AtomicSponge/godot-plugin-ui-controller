89be920f209bab1446105c91a61a48b7ff158ffa
Matthew Evans <mevans+git@wtfsystems.net>
2025-08-10 13:33:48 -0400

    sync

--- addons/ui_controller/plugin.gd
+++ addons/ui_controller/plugin.gd
@@ -6,7 +6,7 @@ const NODE_NAME = "UiAnchor"
 const PATH = "res://addons/ui_controller/"
 
 func _enter_tree() -> void:
-	add_autoload_singleton(AUTOLOAD_NAME, str(PATH + "ui_controller.tscn"))
+	add_autoload_singleton(AUTOLOAD_NAME, str(PATH + "ui_controller.gd"))
 	add_custom_type(NODE_NAME, "Node", preload(PATH + "ui_anchor.gd"), preload(PATH + "icon.svg"))
 	pass
 
