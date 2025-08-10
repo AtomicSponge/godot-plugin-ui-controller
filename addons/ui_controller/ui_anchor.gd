408ed151a2a2adf0810c28ca4aa14a4883bb2c3d
Matthew Evans <mevans+git@wtfsystems.net>
2025-08-10 13:39:04 -0400

    sync

--- addons/ui_controller/ui_anchor.gd
+++ addons/ui_controller/ui_anchor.gd
@@ -2,8 +2,12 @@ extends Node
 ## A node that the UiController will anchor menus to.
 ##
 ## Before calling the UiController members this node needs to be
-## placed in the main scene.
+## placed in the main scene.  After that build the dictionary
+## of menus that can be called.
 
+## List of available menus and their according scene files.
+## After attaching this node this must be filled in before calling
+## UiController.open_menu("menu_name")
 @export var _menus: Dictionary[String, PackedScene]
 
 func _ready() -> void:
